8fef9b337f98ad22cf692800ff872bf42b3cc165
AHMED <chachia.ahmed25@gmail.com>
2023-01-05 16:14:45 +0100

    checkpoint-react-jsx

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Address() {
+  return (
+    <div>
+      <p>Address:Habib Bourguiba Street</p>
+      <p>Béni khalled, 8021</p>
+    </div>
+  );
+}
+
+export default Address;
